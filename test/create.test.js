2d4fdfede0a17228c525b35419b1a4856ec03b27
Joel Denning <joeldenning@gmail.com>
2020-02-08 11:05:27 -0700

    Adding support for yarn create

---
+++ test/create.test.js
@@ -0,0 +1,17 @@
+const { getNpmArgs } = require("../lib/narn-lib");
+
+describe("narn create", () => {
+  it("can run create-single-spa package with npm", () => {
+    expect(getNpmArgs(["create", "single-spa"])).toEqual([
+      "init",
+      "single-spa"
+    ]);
+  });
+
+  it("can run a create-thing package with npm", () => {
+    expect(getNpmArgs(["create", "single-spa"], true)).toEqual([
+      "init",
+      "single-spa"
+    ]);
+  });
+});
