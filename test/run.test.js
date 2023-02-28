b0a01a59d7cb0f098bc88c18f37b7871d1bb06c6
Paul Irish <commits@paul.irish>
2023-02-28 11:25:09 -0800

    Fix narn run run duplication

--- test/run.test.js
+++ test/run.test.js
@@ -1,8 +1,12 @@
-const { getNpmArgs } = require("../lib/narn-lib");
+const {getNpmArgs} = require('../lib/narn-lib');
 
 // https://github.com/joeldenning/narn/issues/21
-describe("narn run", () => {
-  it("lists all available commands with npm run", () => {
-    expect(getNpmArgs(["run"])).toEqual(["run"]);
+describe('narn run', () => {
+  it('lists all available commands with npm run', () => {
+    expect(getNpmArgs(['run'])).toEqual(['run']);
+  });
+
+  it("doesnt repeat 'run'", () => {
+    expect(getNpmArgs(['run', 'build'])).toEqual(['run', 'build']);
   });
 });
